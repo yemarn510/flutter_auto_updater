0a86f0a1eab47c738e2682c0bb6a07b79f0ef4d9
YeMarn <yemarn.510@gmail.com>
2023-05-31 11:04:20 +0700

    FLUTTER: added built macos

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import auto_updater
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AutoUpdaterPlugin.register(with: registry.registrar(forPlugin: "AutoUpdaterPlugin"))
 }
